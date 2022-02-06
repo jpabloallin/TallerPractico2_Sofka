70a293e1941e5e7eed5ad18a217720f272fa607d
jpabloallin <jpac.647@gmail.com>
2022-02-06 14:49:32 -0500

    Creación de la clase Nave Espacial sin tripulantes
    Clase Nave Espacial sin Tripulantes que extiende de la clase Nave Espacial

---
+++ UnmannedSpaceship.java
@@ -0,0 +1,30 @@
+public class UnmannedSpaceship extends Spaceship{
+
+    private String destinationPlanet;
+
+    public UnmannedSpaceship(String name, String weight, String country, String destinationPlanet) {
+        super(name, weight, country);
+        this.destinationPlanet = destinationPlanet;
+    }
+
+    public String getDestinationPlanet() {
+        return destinationPlanet;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() +  "destinationPlanet: " + destinationPlanet;
+    }
+
+    public String propose(){
+        return "su principal objetivo es estudiar otros cuerpos celestes.";
+    }
+
+    
+
+    
+    
+    
+
+    
+}
